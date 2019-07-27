2add8ce4943817bb24f724f19e48cccdd569f6ec
vagrant <vagrant@debian9.localdomain>
2019-07-27 12:12:24 -0700

    Create a henlo theme and setup the base content and style

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var exec          = require('child_process').exec;
 var gulp          = require('gulp');
 var browserSync   = require('browser-sync').create();
+var flatten       = require('gulp-flatten');
 
 gulp.task('browser-sync', function(cb) {
   browserSync.init({
@@ -14,16 +15,51 @@ gulp.task('browser-sync', function(cb) {
 
 gulp.task('build', function(cb) {
   exec('yarn hackmyresume:build', function (err, stdout, stderr) {
-    cb(err);
+    if (err) {
+      console.error(`exec error: ${err}`);
+
+      return cb();
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+
+    return cb();
   });
 });
 
 gulp.task('watch', function(cb) {
-  gulp.watch('src/**/*', {usePolling: true}, gulp.series([
+  gulp.watch([
+    'src/**/*.hbs',
+    'src/**/*.html',
+    'src/**/*.json',
+  ], {usePolling: true}, gulp.series([
       'build',
-      browserSync.reload,
+      (done) => {
+        browserSync.reload();
+        done();
+      },
     ])
-  )
+  );
+
+  // Avoid rebuilding everything for css changes !
+  gulp.watch([
+    'src/**/*.css',
+  ], {usePolling: true}, gulp.series([
+      (done) => {
+        gulp
+          .src('./src/**/html.css')
+          .pipe(flatten())
+          .pipe(gulp.dest('./dist/'))
+          .pipe(browserSync.stream())
+        ;
+
+        done();
+      },
+    ])
+  );
+
+  cb();
 });
 
 gulp.task('default', gulp.series('build', gulp.parallel(['browser-sync', 'watch'])));
